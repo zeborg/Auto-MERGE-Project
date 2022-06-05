58eb4e9066add3d5209565e7a7b4f96f12dd762d
Abhinav Sinha <abhinav@nirmata.com>
2022-06-06 02:41:50 +0530

    Updated workflow
    Signed-off-by: Abhinav Sinha <abhinav@nirmata.com>

--- ci/validate.py
+++ ci/validate.py
@@ -8,6 +8,6 @@ for f in files:
     with open(os.path.join(os.getcwd(),'contributors/'+f)) as fh:
         jsonfile = json.loads(fh.read())
         if sorted(list(jsonfile.keys())) == ['about', 'batch', 'course', 'institution', 'name', 'skills_hobbies']:
-            os.system(f"gh pr merge")
+            os.system(f"gh pr merge -m")
         else:
             sys.exit(f"'{'contributors/'+f}' is either missing one or more required keys, or contains inappropriate keys.")
